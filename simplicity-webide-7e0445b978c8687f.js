9bfee96297c3dab7f4101be8f585f34607ba7968
Christian Lewe <ch_lewe@agdsn.me>
2023-11-29 16:35:35 +0100

    Deploy

--- simplicity-webide-7e0445b978c8687f.js
+++ simplicity-webide-7e0445b978c8687f.js
@@ -491,12 +491,12 @@ imports.wbg.__wbindgen_debug_string = function(arg0, arg1) {
 imports.wbg.__wbindgen_throw = function(arg0, arg1) {
     throw new Error(getStringFromWasm0(arg0, arg1));
 };
-imports.wbg.__wbindgen_closure_wrapper763 = function(arg0, arg1, arg2) {
-    const ret = makeMutClosure(arg0, arg1, 158, __wbg_adapter_18);
+imports.wbg.__wbindgen_closure_wrapper643 = function(arg0, arg1, arg2) {
+    const ret = makeMutClosure(arg0, arg1, 124, __wbg_adapter_18);
     return addHeapObject(ret);
 };
-imports.wbg.__wbindgen_closure_wrapper4778 = function(arg0, arg1, arg2) {
-    const ret = makeMutClosure(arg0, arg1, 1012, __wbg_adapter_21);
+imports.wbg.__wbindgen_closure_wrapper4823 = function(arg0, arg1, arg2) {
+    const ret = makeMutClosure(arg0, arg1, 1024, __wbg_adapter_21);
     return addHeapObject(ret);
 };
 
@@ -537,7 +537,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('simplicity-webide-ca9cc5b33aedddcb_bg.wasm', import.meta.url);
+        input = new URL('simplicity-webide-7e0445b978c8687f_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
