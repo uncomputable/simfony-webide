61a5c61ee4507a7abd8b432b68a8066106a3fe25
Christian Lewe <ch_lewe@agdsn.me>
2023-11-29 15:47:10 +0100

    App: Display past and future instructions
    Move rendering of complex components into separate methods.
    Remove the unnecessary counter.

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use leptos::*;
 use simplicity::jet::Elements;
 
@@ -8,69 +7,99 @@ use crate::util;
 
 #[component]
 pub fn App() -> impl IntoView {
-    let (step, set_step) = create_signal(0);
-    let (output, set_output) = create_signal("(No output)".to_string());
-    let (mac, set_mac) = create_signal(Option::<BitMachine>::None);
-    let (_, set_runner) = create_signal(Option::<Runner<Elements>>::None);
+    let (status, set_status) = create_signal("(Idle)".to_string());
+    let (prefix, set_prefix) = create_signal::<Vec<String>>(vec![]);
+    let (suffix, set_suffix) = create_signal::<Vec<String>>(vec![]);
+
+    let (mac, set_mac) = create_signal::<Option<BitMachine>>(None);
+    let (_, set_runner) = create_signal::<Option<Runner<Elements>>>(None);
 
     let update_program = move |new_human: String| match util::program_from_string(&new_human) {
         Ok(program) => {
             set_mac.set(Some(BitMachine::for_program()));
             let runner = Runner::for_program(program, false);
-            let stack = runner.get_stack().iter().map(|x| x.to_string()).join(" ");
+            let stack = runner.get_stack().iter().map(|x| x.to_string()).collect();
             set_runner.set(Some(runner));
-            set_output.set(stack);
+
+            set_status.set("(Let's start)".to_string());
+            set_prefix.set(vec![]);
+            set_suffix.set(stack);
         }
         Err(error) => {
-            set_output.set(error);
+            set_status.set(format!("Error: {error}"));
         }
-    };
-
-    let display_mac = move || {
-        mac.get()
-            .map(|m| m.to_string())
-            .unwrap_or("(No machine)".to_string())
     };
 
     let run_next_step = move || {
-        set_output.update(|o| {
-            set_mac.update(|maybe_m| {
-                set_runner.update(|maybe_r| {
-                    if let (Some(m), Some(r)) = (maybe_m, maybe_r) {
-                        let status = match r.next(m) {
-                            Ok(Some(x)) => x.to_string(),
-                            Ok(None) => "(Done)".to_string(),
-                            Err(error) => format!("Error: {error}"),
-                        };
-                        let stack = r.get_stack().iter().map(|x| x.to_string()).join(" ");
-                        *o = format!("{stack} | {status}");
-                    }
-                })
+        set_mac.update(|maybe_m| {
+            set_runner.update(|maybe_r| {
+                if let (Some(m), Some(r)) = (maybe_m, maybe_r) {
+                    match r.next(m) {
+                        Ok(Some(instruction)) => {
+                            set_prefix.update(|p| p.push(instruction.to_string()));
+                            set_status.set("(Ok)".to_string());
+                        }
+                        Ok(None) => {
+                            set_status.set("(Done)".to_string());
+                        }
+                        Err(error) => {
+                            set_status.set(format!("Error: {error}"));
+                        }
+                    };
+                    let stack = r.get_stack().iter().map(|x| x.to_string()).collect();
+                    set_suffix.set(stack);
+                }
             })
-        });
-        set_step.update(|n| *n += 1);
+        })
     };
 
     view! {
-        <button
-            on:click=move |_| run_next_step()
-        >
+        <button on:click=move |_| run_next_step()>
             "Next"
         </button>
         <p>
-            <strong>"Step: "</strong>
-            {step}
+            {status}
         </p>
-        <p>
-            {output}
-        </p>
-        <p>
-            {display_mac}
-        </p>
+        <BitMachine mac=mac/>
+        <PastInstructions prefix=prefix/>
+        <InstructionStack suffix=suffix/>
         <textarea
             on:input=move |event| update_program(event_target_value(&event))
             placeholder="Enter program text here"
             rows="10" cols="50"
         />
+    }
+}
+
+#[component]
+fn BitMachine(mac: ReadSignal<Option<BitMachine>>) -> impl IntoView {
+    view! {
+        <p>
+            {
+                move || mac.get().map(|m| m.to_string()).unwrap_or("(No machine)".to_string())
+            }
+        </p>
+    }
+}
+
+#[component]
+fn PastInstructions(prefix: ReadSignal<Vec<String>>) -> impl IntoView {
+    view! {
+        <div class="instructions">
+            {
+                move || prefix.get().iter().map(|s| view! { <span class="instruction">{s}</span> }).collect_view()
+            }
+        </div>
+    }
+}
+
+#[component]
+fn InstructionStack(suffix: ReadSignal<Vec<String>>) -> impl IntoView {
+    view! {
+        <div class="instructions instruction-stack">
+            {
+                move || suffix.get().iter().rev().map(|s| view! { <span class="instruction">{s}</span> }).collect_view()
+            }
+        </div>
     }
 }
