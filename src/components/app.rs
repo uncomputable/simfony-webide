d9c279bc8136bb1b7cbfc6640af1999be44471bc
Christian Lewe <ch_lewe@agdsn.me>
2023-11-30 13:21:04 +0100

    App: Use CachedRunner

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,28 +1,31 @@
 use leptos::*;
 use simplicity::jet::Elements;
 
-use crate::instruction::Runner;
+use crate::instruction::CachedRunner;
 use crate::util;
 
 #[component]
 pub fn App() -> impl IntoView {
     let (status, set_status) = create_signal("(Idle)".to_string());
-    let (prefix, set_prefix) = create_signal::<Vec<String>>(vec![]);
-    let (suffix, set_suffix) = create_signal::<Vec<String>>(vec![]);
-
-    let (runner, set_runner) = create_signal::<Option<Runner<Elements>>>(None);
+    let (runner, set_runner) = create_signal::<Option<CachedRunner<Elements>>>(None);
+    let past_instructions = Signal::<Option<Vec<String>>>::derive(move || {
+        runner
+            .get()
+            .map(|r| r.past_instructions().map(|i| i.to_string()).collect())
+    });
+    let next_instructions = Signal::<Option<Vec<String>>>::derive(move || {
+        runner
+            .get()
+            .map(|r| r.next_instructions().map(|i| i.to_string()).collect())
+    });
 
     let update_program = move |human: String, tco: bool| match util::program_from_string(&human) {
         Ok(program) => {
-            let runner = Runner::for_program(program, tco);
-            let stack = runner.get_stack().iter().map(|x| x.to_string()).collect();
-            set_runner.set(Some(runner));
-
+            set_runner.set(Some(CachedRunner::for_program(program, tco)));
             set_status.set("(Let's start)".to_string());
-            set_prefix.set(vec![]);
-            set_suffix.set(stack);
         }
         Err(error) => {
+            set_runner.set(None);
             set_status.set(format!("Error: {error}"));
         }
     };
@@ -31,19 +34,12 @@ pub fn App() -> impl IntoView {
         set_runner.update(|maybe_r| {
             if let Some(r) = maybe_r {
                 match r.next() {
-                    Ok(Some(instruction)) => {
-                        set_prefix.update(|p| p.push(instruction.to_string()));
-                        set_status.set("(Ok)".to_string());
+                    Ok(()) if r.next_instructions().next().is_none() => {
+                        set_status.set("(Finished)".to_string())
                     }
-                    Ok(None) => {
-                        set_status.set("(Done)".to_string());
-                    }
-                    Err(error) => {
-                        set_status.set(format!("Error: {error}"));
-                    }
+                    Ok(()) => set_status.set("(Ok)".to_string()),
+                    Err(error) => set_status.set(format!("Error: {error}")),
                 };
-                let stack = r.get_stack().iter().map(|x| x.to_string()).collect();
-                set_suffix.set(stack);
             }
         })
     };
@@ -56,8 +52,8 @@ pub fn App() -> impl IntoView {
             {status}
         </p>
         <BitMachine runner=runner/>
-        <PastInstructions prefix=prefix/>
-        <InstructionStack suffix=suffix/>
+        <Instructions instructions=past_instructions highlight=false/>
+        <Instructions instructions=next_instructions highlight=true/>
         <textarea
             on:input=move |event| update_program(event_target_value(&event), false)
             placeholder="Enter program text here"
@@ -67,7 +63,7 @@ pub fn App() -> impl IntoView {
 }
 
 #[component]
-fn BitMachine(runner: ReadSignal<Option<Runner<Elements>>>) -> impl IntoView {
+fn BitMachine(runner: ReadSignal<Option<CachedRunner<Elements>>>) -> impl IntoView {
     view! {
         <p>
             {
@@ -78,22 +74,16 @@ fn BitMachine(runner: ReadSignal<Option<Runner<Elements>>>) -> impl IntoView {
 }
 
 #[component]
-fn PastInstructions(prefix: ReadSignal<Vec<String>>) -> impl IntoView {
+fn Instructions(instructions: Signal<Option<Vec<String>>>, highlight: bool) -> impl IntoView {
+    let class = if highlight {
+        "instructions next_instructions"
+    } else {
+        "instructions"
+    };
     view! {
-        <div class="instructions">
+        <div class=class>
             {
-                move || prefix.get().iter().map(|s| view! { <span class="instruction">{s}</span> }).collect_view()
-            }
-        </div>
-    }
-}
-
-#[component]
-fn InstructionStack(suffix: ReadSignal<Vec<String>>) -> impl IntoView {
-    view! {
-        <div class="instructions instruction-stack">
-            {
-                move || suffix.get().iter().rev().map(|s| view! { <span class="instruction">{s}</span> }).collect_view()
+                move || instructions.get().map(|is| is.iter().map(|i| view! { <span class="instruction">{i}</span> }).collect_view())
             }
         </div>
     }
