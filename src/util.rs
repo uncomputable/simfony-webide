3940dfc8fe291199737afd32ef7b793bfa11e8f9
Christian Lewe <ch_lewe@agdsn.me>
2023-12-19 13:18:16 +0100

    Util: Add DisplayInner
    Type to display the label of a node in a prettier way.
    The label doesn't include any parentheses.
    Adapt DisplayExpression to use DisplayInner.

--- src/util.rs
+++ src/util.rs
@@ -19,6 +19,43 @@ pub fn program_from_string(s: &str) -> Result<Arc<RedeemNode<Elements>>, String>
         .map_err(|e| e.to_string())
 }
 
+pub struct DisplayInner<'a, M: node::Marker>(&'a node::Node<M>);
+
+impl<'a, M: node::Marker> From<&'a node::Node<M>> for DisplayInner<'a, M> {
+    fn from(node: &'a node::Node<M>) -> Self {
+        Self(node)
+    }
+}
+
+impl<'a, M: node::Marker> fmt::Display for DisplayInner<'a, M> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.0.inner() {
+            Inner::Iden => f.write_str("iden"),
+            Inner::Unit => f.write_str("unit"),
+            Inner::InjL(_) => f.write_str("injl"),
+            Inner::InjR(_) => f.write_str("injr"),
+            Inner::Take(_) => f.write_str("take"),
+            Inner::Drop(_) => f.write_str("drop"),
+            Inner::Comp(_, _) => f.write_str("comp"),
+            Inner::Case(_, _) => f.write_str("case"),
+            Inner::AssertL(_, _) => f.write_str("assertl"),
+            Inner::AssertR(_, _) => f.write_str("assertr"),
+            Inner::Pair(_, _) => f.write_str("pair"),
+            Inner::Disconnect(_, _) => f.write_str("disconnect"),
+            Inner::Witness(_) => f.write_str("witness"),
+            Inner::Fail(_) => f.write_str("fail"),
+            Inner::Jet(jet) => write!(f, "jet_{}", jet),
+            Inner::Word(value) => write!(f, "const {}", ExtValue::from(value.as_ref())),
+        }
+    }
+}
+
+impl<'a, M: node::Marker> fmt::Debug for DisplayInner<'a, M> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(self, f)
+    }
+}
+
 pub struct DisplayExpression<'a, M: node::Marker>(&'a node::Node<M>);
 
 impl<'a, M: node::Marker> From<&'a node::Node<M>> for DisplayExpression<'a, M> {
@@ -35,45 +72,26 @@ where
         for data in self.0.verbose_pre_order_iter::<NoSharing>() {
             match data.n_children_yielded {
                 1 => {
-                    debug_assert!(matches!(
-                        data.node.inner().as_dag(),
-                        Dag::Unary(..) | Dag::Binary(..)
-                    ));
                     if let Dag::Binary(..) = data.node.inner().as_dag() {
                         f.write_str(" ")?;
+                    } else {
+                        debug_assert!(matches!(data.node.inner().as_dag(), Dag::Unary(..)));
                     }
-                    continue;
                 }
                 2 => {
                     debug_assert!(matches!(data.node.inner().as_dag(), Dag::Binary(..)));
                     f.write_str(")")?;
-                    continue;
                 }
                 n => {
                     debug_assert!(n == 0, "Combinators are nullary, unary or binary");
+                    write!(f, "{}", DisplayInner::from(data.node))?;
+                    match data.node.inner().as_dag() {
+                        Dag::Unary(..) => f.write_str(" ")?,
+                        Dag::Binary(..) => f.write_str(" (")?,
+                        _ => {}
+                    }
                 }
             };
-
-            match data.node.inner() {
-                Inner::Iden => f.write_str("iden")?,
-                Inner::Unit => f.write_str("unit")?,
-                Inner::InjL(_) => f.write_str("injl")?,
-                Inner::InjR(_) => f.write_str("injr")?,
-                Inner::Take(_) => f.write_str("take")?,
-                Inner::Drop(_) => f.write_str("drop")?,
-                Inner::Comp(_, _) => f.write_str("comp (")?,
-                Inner::Case(_, _) => f.write_str("case (")?,
-                Inner::AssertL(_, _) => f.write_str("assertl")?,
-                Inner::AssertR(_, _) => f.write_str("assertr")?,
-                Inner::Pair(_, _) => f.write_str("pair (")?,
-                Inner::Disconnect(_, _) => f.write_str("disconnect (")?,
-                Inner::Witness(_) => f.write_str("witness")?,
-                Inner::Fail(_) => f.write_str("fail")?,
-                Inner::Jet(jet) => write!(f, "jet_{}", jet)?,
-                Inner::Word(value) => {
-                    write!(f, "const {}", ExtValue::from(value.as_ref()))?;
-                }
-            }
         }
 
         Ok(())
