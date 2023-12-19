ba6c12d743db205b7a8737092b25e8ecd35324e8
Christian Lewe <ch_lewe@agdsn.me>
2023-12-19 17:47:52 +0100

    Nix: Reformat flake

--- flake.nix
+++ flake.nix
@@ -15,7 +15,9 @@
   , ...
   }:
   flake-utils.lib.eachSystem [
-    "x86_64-linux" "aarch64-linux" "x86_64-darwin"
+    "x86_64-linux"
+    "aarch64-linux"
+    "x86_64-darwin"
   ] (system:
     let
       overlays = [
