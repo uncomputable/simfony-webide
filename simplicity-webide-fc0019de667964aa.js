5e9021a8fd7f1e9a152f95c070a35b3677b95edf
uncomputable <ch_lewe@agdsn.me>
2023-12-20 16:15:30 +0000

    Deploying to pages from @ uncomputable/simplicity-webide@47f344da0acca702e7ba8894e71b6d04e682aa62 🚀

--- simplicity-webide-fc0019de667964aa.js
+++ simplicity-webide-fc0019de667964aa.js
@@ -534,7 +534,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('simplicity-webide-a259d7d48673c8d2_bg.wasm', import.meta.url);
+        input = new URL('simplicity-webide-fc0019de667964aa_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
