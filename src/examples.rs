6cc13f119c2a804abe92d3a27deefe466bd03ec1
Christian Lewe <ch_lewe@agdsn.me>
2024-01-30 23:54:27 +0100

    Examples: Add Schnorr

--- src/examples.rs
+++ src/examples.rs
@@ -1,5 +1,5 @@
 // Names must be unique because they serve as primary keys
-pub(crate) const NAME_TO_PROGRAM: [(&str, &str); 10] = [
+pub(crate) const NAME_TO_PROGRAM: [(&str, &str); 11] = [
     ("unit", UNIT),
     ("iden", IDEN),
     ("not", NOT),
@@ -10,13 +10,15 @@ pub(crate) const NAME_TO_PROGRAM: [(&str, &str); 10] = [
     ("assertl failure", ASSERTL_FAILURE),
     ("jet_one failure", JET_ONE_FAILURE),
     ("byte equality failure", BYTE_EQUALITY),
+    ("Schnorr signature failure", SCHNORR),
 ];
 
 #[rustfmt::skip]
-pub(crate) const NAME_TO_DESCRIPTION: [(&str, &str); 3] = [
+pub(crate) const NAME_TO_DESCRIPTION: [(&str, &str); 4] = [
     ("unit", UNIT_DESCRIPTION),
     ("iden", IDEN_DESCRIPTION),
     ("byte equality failure", BYTE_EQUALITY_DESCRIPTION),
+    ("Schnorr signature failure", SCHNORR_DESCRIPTION),
 ];
 
 pub fn get_names() -> impl ExactSizeIterator<Item = &'static str> {
@@ -68,6 +70,15 @@ pub const BYTE_EQUALITY: &str = r#"a := const 0x00
 b := const 0x00
 main := comp (comp (pair a b) jet_eq_8) jet_verify"#;
 const BYTE_EQUALITY_DESCRIPTION: &str = r#"Succeeds if a and b are equal and fails otherwise."#;
+
+pub const SCHNORR: &str = r#"sig := const 0xe907831f80848d1069a5371b402410364bdf1c5f8307b0084c55f1ce2dca821525f66a4a85ea8b71e482a74f382d2ce5ebeee8fdb2172f477df4900d310536c0 : 1 -> 2^512
+pk := const 0xf9308a019258c31049344f85f89d5229b531c845836f99b08601f113bce036f9 : 1 -> 2^256
+msg := const 0x0000000000000000000000000000000000000000000000000000000000000000 : 1 -> 2^256
+in := pair (pair pk msg) sig : 1 -> 2^512 * 2^512
+out := jet_bip_0340_verify : 2^512 * 2^512 -> 1
+main := comp in out"#;
+const SCHNORR_DESCRIPTION: &str =
+    r#"Succeeds if the Schnorr signature matches the public key and message, and fails otherwise."#;
 
 #[cfg(test)]
 mod tests {
