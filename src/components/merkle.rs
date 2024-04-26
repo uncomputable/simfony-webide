f25284a05937c1f7e39c6206e2f88be0e2369f46
Christian Lewe <ch_lewe@agdsn.me>
2024-04-26 14:43:27 +0200

    Merkle: Hide <div> for erratic programs

--- src/components/merkle.rs
+++ src/components/merkle.rs
@@ -8,16 +8,16 @@ use simplicity::dag::DagLike;
 #[component]
 pub fn Merkle(program: Signal<Result<Arc<Expression>, String>>) -> impl IntoView {
     view! {
-        <div class="merkle">
         {
             move || program.get().ok().map(|t| view! {
-                <h2>Merkle tree</h2>
-                <p>A Simplicity program is a Merkle tree, which makes it easy to analyze.</p>
+                <div class="merkle">
+                    <h2>Merkle tree</h2>
+                    <p>A Simplicity program is a Merkle tree, which makes it easy to analyze.</p>
 
-                <MerkleRec expression=t/>
+                    <MerkleRec expression=t/>
+                </div>
             })
         }
-        </div>
     }
 }
 
@@ -45,16 +45,16 @@ fn MerkleRec(expression: Arc<Expression>) -> impl IntoView {
 #[component]
 pub fn MerkleGraph(program: Signal<Result<Arc<Expression>, String>>) -> impl IntoView {
     view! {
-        <div class="">
         {
             move || program.get().ok().map(|_| view! {
-                <h2>Merkle Graph</h2>
+                <div class="">
+                    <h2>Merkle Graph</h2>
 
-                <div id="merkle_graph_holder">
-                    <svg></svg>
+                    <div id="merkle_graph_holder">
+                        <svg></svg>
+                    </div>
                 </div>
             })
         }
-        </div>
     }
 }
