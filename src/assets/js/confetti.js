74977d02deb719ef9ace0129ba36effa95a426ba
Daniel Petersen <danielpetersen@blockstream.com>
2024-05-12 17:25:26 -0700

    bubble effect and screen flash on success

--- src/assets/js/confetti.js
+++ src/assets/js/confetti.js
@@ -8,4 +8,15 @@ export function start_confetti(){
     setTimeout(function(){
       confetti_div.classList.remove('animate');
     },700);
+
+    flash_screen()
 };
+
+function flash_screen(){
+    document.body.classList.remove("animate-body-flash");
+    document.body.classList.add("animate-body-flash");
+
+    setTimeout(function(){
+        document.body.classList.remove("animate-body-flash");
+      },1000);
+}
