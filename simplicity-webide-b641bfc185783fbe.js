03bb1563e60e1514dc32288d4c7e3817b3ee1c90
uncomputable <ch_lewe@agdsn.me>
2024-05-15 16:52:05 +0000

    Deploying to pages from @ uncomputable/simfony-webide@7fb759110afbababf5f451cec29a058abde3bc38 🚀

--- simplicity-webide-b641bfc185783fbe.js
+++ simplicity-webide-b641bfc185783fbe.js
@@ -571,7 +571,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('simplicity-webide-46e8ca608f281ccf_bg.wasm', import.meta.url);
+        input = new URL('simplicity-webide-b641bfc185783fbe_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
