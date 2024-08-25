6769407d8c7aec5c4bd9005b54a146e4f0290593
Christian Lewe <ch_lewe@agdsn.me>
2024-08-25 10:48:09 +0200

    chore: Update flake to NixOS 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Simplicity web IDE";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
