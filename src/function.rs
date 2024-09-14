20d1e5acc00299f70c9d9c4a008e3278f4127861
Christian Lewe <ch_lewe@agdsn.me>
2024-09-14 15:29:49 +0200

    refactor: Remove State
    It's simpler to keep a stack of input values and a stack of tasks,
    as opposed to a stack of states. We can get rid of a lot of calls to
    Clone::clone. Join execute_state with run.

--- src/function.rs
+++ src/function.rs
@@ -10,23 +10,6 @@ use crate::jet::JetFailed;
 use crate::simplicity;
 use crate::util::Expression;
 
-#[derive(Debug, Clone, Eq, PartialEq)]
-pub struct State {
-    pub expression: Arc<Expression>,
-    pub input: Value,
-}
-
-impl fmt::Display for State {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self.input.as_product().is_some() {
-            true => {
-                write!(f, "[{}]{}", self.expression.display_expr(), self.input)
-            }
-            false => write!(f, "[{}]({})", self.expression.display_expr(), self.input),
-        }
-    }
-}
-
 #[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub enum ErrorKind {
     AssertionFailed,
@@ -50,9 +33,9 @@ impl fmt::Display for ErrorKind {
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 enum Task {
-    Execute(State),
-    ExecuteComp(Arc<Expression>),
-    ExecuteDisconnect(Arc<Expression>),
+    Execute(Arc<Expression>),
+    MoveLeftCompOutput,
+    MoveLeftDisconnectOutput,
     MakeLeft(Arc<Final>),
     MakeRight(Arc<Final>),
     MakeProduct,
@@ -61,46 +44,126 @@ enum Task {
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Runner {
     /// Stack of tasks to run.
-    input: Vec<Task>,
-    /// Stack of produced outputs.
+    tasks: Vec<Task>,
+    /// Stack of input values.
+    input: Vec<Value>,
+    /// Stack of output values.
     output: Vec<Value>,
 }
 
 impl Runner {
     pub fn for_program(program: Arc<Expression>) -> Self {
-        let initial_state = State {
-            expression: program,
-            input: Value::unit(),
-        };
         Self {
-            input: vec![Task::Execute(initial_state)],
+            tasks: vec![Task::Execute(program)],
+            input: vec![Value::unit()],
             output: vec![],
         }
     }
 
     pub fn run(&mut self) -> Result<Value, ErrorKind> {
-        while let Some(task) = self.input.pop() {
+        while let Some(task) = self.tasks.pop() {
             match task {
-                Task::Execute(state) => {
-                    self.execute_state(state)?;
+                Task::Execute(expression) => {
+                    let input = self.input.pop().unwrap();
+                    match expression.inner() {
+                        Inner::Iden => self.output.push(input),
+                        Inner::Unit => self.output.push(Value::unit()),
+                        Inner::InjL(t) => {
+                            let ty_r = expression.arrow().target.as_sum().unwrap().1;
+                            self.tasks.push(Task::MakeLeft(Arc::new(ty_r.clone())));
+                            self.tasks.push(Task::Execute(Arc::clone(t)));
+                            self.input.push(input);
+                        }
+                        Inner::InjR(t) => {
+                            let ty_l = expression.arrow().target.as_sum().unwrap().0;
+                            self.tasks.push(Task::MakeRight(Arc::new(ty_l.clone())));
+                            self.tasks.push(Task::Execute(Arc::clone(t)));
+                            self.input.push(input);
+                        }
+                        Inner::Take(t) => {
+                            let (a, _) = input.as_product().ok_or(ErrorKind::WrongType)?;
+                            self.tasks.push(Task::Execute(Arc::clone(t)));
+                            self.input.push(a.shallow_clone());
+                        }
+                        Inner::Drop(t) => {
+                            let (_, b) = input.as_product().ok_or(ErrorKind::WrongType)?;
+                            self.tasks.push(Task::Execute(Arc::clone(t)));
+                            self.input.push(b.shallow_clone());
+                        }
+                        Inner::Comp(s, t) => {
+                            self.tasks.push(Task::Execute(Arc::clone(t)));
+                            self.tasks.push(Task::MoveLeftCompOutput);
+                            self.tasks.push(Task::Execute(Arc::clone(s)));
+                            self.input.push(input);
+                        }
+                        Inner::Pair(s, t) => {
+                            self.tasks.push(Task::MakeProduct);
+                            self.tasks.push(Task::Execute(Arc::clone(t)));
+                            self.tasks.push(Task::Execute(Arc::clone(s)));
+                            self.input.push(input.shallow_clone());
+                            self.input.push(input);
+                        }
+                        Inner::Case(..) | Inner::AssertL(..) | Inner::AssertR(..) => {
+                            let (sum_a_b, c) = input.as_product().ok_or(ErrorKind::WrongType)?;
+
+                            if let Some(a) = sum_a_b.as_left() {
+                                match expression.inner() {
+                                    Inner::Case(s, _) | Inner::AssertL(s, _) => {
+                                        self.tasks.push(Task::Execute(Arc::clone(s)));
+                                        self.input.push(Value::product(
+                                            a.shallow_clone(),
+                                            c.shallow_clone(),
+                                        ));
+                                    }
+                                    Inner::AssertR(_, _) => return Err(ErrorKind::AssertionFailed),
+                                    _ => unreachable!("Covered by outer match statement"),
+                                }
+                            } else if let Some(b) = sum_a_b.as_right() {
+                                match expression.inner() {
+                                    Inner::Case(_, t) | Inner::AssertR(_, t) => {
+                                        self.tasks.push(Task::Execute(Arc::clone(t)));
+                                        self.input.push(Value::product(
+                                            b.shallow_clone(),
+                                            c.shallow_clone(),
+                                        ));
+                                    }
+                                    Inner::AssertL(_, _) => return Err(ErrorKind::AssertionFailed),
+                                    _ => unreachable!("Covered by outer match statement"),
+                                }
+                            } else {
+                                return Err(ErrorKind::WrongType);
+                            }
+                        }
+                        Inner::Disconnect(s, t) => {
+                            self.tasks.push(Task::MakeProduct);
+                            self.tasks.push(Task::Execute(Arc::clone(t)));
+                            self.tasks.push(Task::MoveLeftDisconnectOutput);
+                            self.tasks.push(Task::Execute(Arc::clone(s)));
+                            let t_cmr = Value::u256(t.cmr().to_byte_array());
+                            self.input.push(Value::product(t_cmr, input));
+                        }
+                        Inner::Witness(value) => self.output.push(value.shallow_clone()),
+                        Inner::Fail(_) => return Err(ErrorKind::FailNode),
+                        Inner::Jet(jet) => match jet::execute_jet_with_env(
+                            jet,
+                            &input,
+                            &simfony::dummy_env::dummy(),
+                        ) {
+                            Ok(output) => self.output.push(output),
+                            Err(JetFailed) => return Err(ErrorKind::JetFailed),
+                        },
+                        Inner::Word(value) => self.output.push(value.shallow_clone()),
+                    }
                 }
-                Task::ExecuteComp(t) => {
-                    let input = self.output.pop().unwrap();
-                    let state = State {
-                        expression: t,
-                        input,
-                    };
-                    self.execute_state(state)?;
+                Task::MoveLeftCompOutput => {
+                    let output = self.output.pop().unwrap();
+                    self.input.push(output);
                 }
-                Task::ExecuteDisconnect(t) => {
+                Task::MoveLeftDisconnectOutput => {
                     let prod_b_c = self.output.pop().unwrap();
                     let (b, c) = prod_b_c.as_product().unwrap();
                     self.output.push(b.shallow_clone());
-                    let state = State {
-                        expression: t,
-                        input: c.shallow_clone(),
-                    };
-                    self.execute_state(state)?;
+                    self.input.push(c.shallow_clone());
                 }
                 Task::MakeLeft(ty_r) => {
                     let val_l = self.output.pop().unwrap();
@@ -118,138 +181,9 @@ impl Runner {
             }
         }
 
+        debug_assert!(self.input.is_empty());
         debug_assert_eq!(self.output.len(), 1);
         Ok(self.output.pop().unwrap())
-    }
-
-    fn execute_state(&mut self, state: State) -> Result<(), ErrorKind> {
-        let inner = state.expression.inner();
-        match inner {
-            Inner::Iden => {
-                self.output.push(state.input);
-            }
-            Inner::Unit => {
-                self.output.push(Value::unit());
-            }
-            Inner::InjL(t) => {
-                let ty_r = state.expression.arrow().target.as_sum().unwrap().1;
-                self.input.push(Task::MakeLeft(Arc::new(ty_r.clone())));
-                let t_state = State {
-                    expression: t.clone(),
-                    input: state.input,
-                };
-                self.input.push(Task::Execute(t_state));
-            }
-            Inner::InjR(t) => {
-                let ty_l = state.expression.arrow().target.as_sum().unwrap().0;
-                self.input.push(Task::MakeRight(Arc::new(ty_l.clone())));
-                let t_state = State {
-                    expression: t.clone(),
-                    input: state.input,
-                };
-                self.input.push(Task::Execute(t_state));
-            }
-            Inner::Take(t) => {
-                let (a, _) = state.input.as_product().ok_or(ErrorKind::WrongType)?;
-                let t_state = State {
-                    expression: t.clone(),
-                    input: a.shallow_clone(),
-                };
-                self.input.push(Task::Execute(t_state));
-            }
-            Inner::Drop(t) => {
-                let (_, b) = state.input.as_product().ok_or(ErrorKind::WrongType)?;
-                let t_state = State {
-                    expression: t.clone(),
-                    input: b.shallow_clone(),
-                };
-                self.input.push(Task::Execute(t_state));
-            }
-            Inner::Comp(s, t) => {
-                self.input.push(Task::ExecuteComp(t.clone()));
-                let s_state = State {
-                    expression: s.clone(),
-                    input: state.input,
-                };
-                self.input.push(Task::Execute(s_state));
-            }
-            Inner::Pair(s, t) => {
-                self.input.push(Task::MakeProduct);
-                let t_state = State {
-                    expression: t.clone(),
-                    input: state.input.clone(),
-                };
-                self.input.push(Task::Execute(t_state));
-                let s_state = State {
-                    expression: s.clone(),
-                    input: state.input,
-                };
-                self.input.push(Task::Execute(s_state));
-            }
-            Inner::Case(..) | Inner::AssertL(..) | Inner::AssertR(..) => {
-                let (sum_a_b, c) = state.input.as_product().ok_or(ErrorKind::WrongType)?;
-
-                if let Some(a) = sum_a_b.as_left() {
-                    match inner {
-                        Inner::Case(s, _) | Inner::AssertL(s, _) => {
-                            let s_state = State {
-                                expression: s.clone(),
-                                input: Value::product(a.shallow_clone(), c.shallow_clone()),
-                            };
-                            self.input.push(Task::Execute(s_state));
-                        }
-                        Inner::AssertR(_, _) => {
-                            return Err(ErrorKind::AssertionFailed);
-                        }
-                        _ => unreachable!("Covered by outer match statement"),
-                    }
-                } else if let Some(b) = sum_a_b.as_right() {
-                    match inner {
-                        Inner::Case(_, t) | Inner::AssertR(_, t) => {
-                            let t_state = State {
-                                expression: t.clone(),
-                                input: Value::product(b.shallow_clone(), c.shallow_clone()),
-                            };
-                            self.input.push(Task::Execute(t_state));
-                        }
-                        Inner::AssertL(_, _) => {
-                            return Err(ErrorKind::AssertionFailed);
-                        }
-                        _ => unreachable!("Covered by outer match statement"),
-                    }
-                } else {
-                    return Err(ErrorKind::WrongType);
-                }
-            }
-            Inner::Disconnect(s, t) => {
-                self.input.push(Task::MakeProduct);
-                self.input.push(Task::ExecuteDisconnect(t.clone()));
-
-                let t_cmr = Value::u256(t.cmr().to_byte_array());
-                let s_state = State {
-                    expression: s.clone(),
-                    input: Value::product(t_cmr, state.input),
-                };
-                self.input.push(Task::Execute(s_state));
-            }
-            Inner::Witness(value) => self.output.push(value.shallow_clone()),
-            Inner::Fail(_) => {
-                return Err(ErrorKind::FailNode);
-            }
-            Inner::Jet(jet) => {
-                match jet::execute_jet_with_env(jet, &state.input, &simfony::dummy_env::dummy()) {
-                    Ok(output) => {
-                        self.output.push(output);
-                    }
-                    Err(JetFailed) => {
-                        return Err(ErrorKind::JetFailed);
-                    }
-                }
-            }
-            Inner::Word(value) => self.output.push(value.shallow_clone()),
-        };
-
-        Ok(())
     }
 }
 
