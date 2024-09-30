17450e6f7b84efe106c0faf6d7180af3c15ced95
Christian Lewe <ch_lewe@agdsn.me>
2024-09-30 17:56:54 +0200

    feat: Show tooltip for copy-to-clipboard buttons

--- src/components/copy_to_clipboard.rs
+++ src/components/copy_to_clipboard.rs
@@ -1,6 +1,4 @@
-use leptos::{
-    component, create_rw_signal, ev, spawn_local, view, IntoView, Show, SignalGet, SignalSet,
-};
+use leptos::{component, create_rw_signal, ev, view, IntoView, SignalSet};
 
 #[component]
 pub fn CopyToClipboard(label: String, content: String) -> impl IntoView {
@@ -9,35 +7,31 @@ pub fn CopyToClipboard(label: String, content: String) -> impl IntoView {
         .map(web_sys::Window::navigator)
         .as_ref()
         .map(web_sys::Navigator::clipboard);
-    let copied = create_rw_signal(false);
 
     match maybe_clipboard {
         Some(clipboard) => {
+            let tooltip_text = create_rw_signal("Copy");
+
             let button_click = move |_event: ev::MouseEvent| {
-                let promise = clipboard.write_text(content.as_str());
-                let future = wasm_bindgen_futures::JsFuture::from(promise);
-                spawn_local(async move {
-                    if future.await.is_ok() {
-                        copied.set(true);
-                        gloo_timers::future::TimeoutFuture::new(500).await;
-                        copied.set(false);
-                    }
-                });
+                let _promise = clipboard.write_text(content.as_str());
+                tooltip_text.set("Copied!");
+            };
+            let button_mouseout = move |_event: ev::MouseEvent| {
+                tooltip_text.set("Copy");
             };
 
             view! {
-                <button
-                    class="copy-button"
-                    on:click=button_click
-                >
-                    <Show
-                        when=move || copied.get()
-                        fallback=move || label.clone()
+                <div class="tooltip">
+                    <button
+                        class="copy-button"
+                        on:click=button_click
+                        on:mouseout=button_mouseout
                     >
-                        "Copied!"
-                    </Show>
-                    <i class="far fa-copy"></i>
-                </button>
+                        <span class="tooltip-text">{tooltip_text}</span>
+                        {label}
+                        <i class="far fa-copy"></i>
+                    </button>
+                </div>
             }
             .into_any()
         }
