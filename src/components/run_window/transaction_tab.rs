4d4cfb9c771410fa7426bf8c46911e52dccfaf58
Christian Lewe <ch_lewe@agdsn.me>
2024-10-20 22:39:21 +0200

    feat: Expose TxParams in TransactionTab

--- src/components/run_window/transaction_tab.rs
+++ src/components/run_window/transaction_tab.rs
@@ -1,8 +1,9 @@
+use std::str::FromStr;
 use std::sync::Arc;
 
 use leptos::{
-    component, create_rw_signal, ev, event_target_value, use_context, view, with, IntoView,
-    RwSignal, Signal, SignalGetUntracked, SignalSet, SignalUpdate,
+    component, create_rw_signal, ev, event_target_value, use_context, view, with, Children,
+    IntoView, RwSignal, Signal, SignalGetUntracked, SignalSet, SignalUpdate,
 };
 use simfony::{elements, simplicity};
 use simplicity::jet::elements::ElementsEnv;
@@ -34,9 +35,50 @@ impl TxEnv {
 #[component]
 pub fn TransactionTab() -> impl IntoView {
     let tx_env = use_context::<TxEnv>().expect("transaction environment should exist in context");
+    let txid_parse_error = create_rw_signal("".to_string());
+    let vout_parse_error = create_rw_signal("".to_string());
+    let recipient_address_parse_error = create_rw_signal("".to_string());
+    let fee_parse_error = create_rw_signal("".to_string());
     let lock_time_parse_error = create_rw_signal("".to_string());
     let sequence_parse_error = create_rw_signal("".to_string());
 
+    let update_txid = move |e: ev::Event| match elements::Txid::from_str(&event_target_value(&e)) {
+        Ok(txid) => {
+            tx_env.params.update(|x| x.txid = txid);
+            txid_parse_error.update(String::clear);
+        }
+        Err(error) => txid_parse_error.set(error.to_string()),
+    };
+    let update_vout = move |e: ev::Event| match event_target_value(&e).parse::<u32>() {
+        Ok(vout) => {
+            tx_env.params.update(|x| x.vout = vout);
+            vout_parse_error.update(String::clear);
+        }
+        Err(error) => vout_parse_error.set(error.to_string()),
+    };
+    let update_recipient_address = move |e: ev::Event| {
+        let s = event_target_value(&e);
+        match elements::Address::parse_with_params(&s, &elements::AddressParams::LIQUID_TESTNET) {
+            Ok(address) => {
+                tx_env
+                    .params
+                    .update(|x| x.recipient_address = Some(address));
+                recipient_address_parse_error.update(String::clear);
+            }
+            Err(..) if s.is_empty() => {
+                tx_env.params.update(|x| x.recipient_address = None);
+                recipient_address_parse_error.update(String::clear);
+            }
+            Err(error) => recipient_address_parse_error.set(error.to_string()),
+        }
+    };
+    let update_fee = move |e: ev::Event| match event_target_value(&e).parse::<u64>() {
+        Ok(fee) => {
+            tx_env.params.update(|x| x.fee = fee);
+            fee_parse_error.update(String::clear);
+        }
+        Err(error) => fee_parse_error.set(error.to_string()),
+    };
     let update_lock_time = move |e: ev::Event| match event_target_value(&e).parse::<u32>() {
         Ok(lock_time) => {
             let lock_time = elements::LockTime::from_consensus(lock_time);
@@ -56,19 +98,49 @@ pub fn TransactionTab() -> impl IntoView {
 
     view! {
         <div class="tab-content transaction-tab">
-            <h3 class="tab-title">
-                Transaction Environment
-            </h3>
             <p class="tab-description">
-                "Currently, the runtime uses a "
-                <a href="https://github.com/BlockstreamResearch/simfony/blob/master/src/dummy_env.rs">
-                    dummy transaction environment</a>.
-                " Only the lock time and sequence number can be changed. "
+                "Only a limited number of fields are available. "
                 "More customization will follow in future updates."
             </p>
-            <div>
-                <div class="transaction-display-row">
-                    <div class="display-row-label">"nLockTime"</div>
+            <Section name="Previous Outpoint">
+                <Item name="txid" error=txid_parse_error>
+                    <input
+                        class="input"
+                        type="text"
+                        on:input=update_txid
+                        value=tx_env.params.get_untracked().txid.to_string()
+                    />
+                </Item>
+                <Item name="vout" error=vout_parse_error>
+                    <input
+                        class="input"
+                        type="number"
+                        min=0
+                        on:input=update_vout
+                        value=tx_env.params.get_untracked().vout
+                    />
+                </Item>
+            </Section>
+            <Section name="Transaction">
+                <Item name="recipient address" error=recipient_address_parse_error>
+                    <input
+                        class="input"
+                        type="text"
+                        on:input=update_recipient_address
+                        value=tx_env.params.get_untracked().recipient_address.as_ref().map(ToString::to_string).unwrap_or_default()
+                    />
+                </Item>
+                <Item name="fee (sats)" error=fee_parse_error>
+                    <input
+                        class="input"
+                        type="number"
+                        on:input=update_fee
+                        min=0
+                        value=10000
+                        value=tx_env.params.get_untracked().fee
+                    />
+                </Item>
+                <Item name="nLockTime" error=lock_time_parse_error>
                     <input
                         class="input"
                         type="number"
@@ -76,10 +148,8 @@ pub fn TransactionTab() -> impl IntoView {
                         min=0
                         value=tx_env.params.get_untracked().lock_time.to_string()
                     />
-                </div>
-                <ErrorBox error=lock_time_parse_error />
-                <div class="transaction-display-row">
-                    <div class="display-row-label">"nSequence"</div>
+                </Item>
+                <Item name="nSequence" error=sequence_parse_error>
                     <input
                         class="input"
                         type="number"
@@ -87,9 +157,37 @@ pub fn TransactionTab() -> impl IntoView {
                         min=0
                         value=tx_env.params.get_untracked().sequence.to_string()
                     />
-                </div>
-                <ErrorBox error=lock_time_parse_error />
-            </div>
+                </Item>
+            </Section>
         </div>
+    }
+}
+
+#[component]
+fn Section(#[prop(into)] name: String, children: Children) -> impl IntoView {
+    view! {
+        <h3 class="tab-title">
+            {name}
+        </h3>
+        <div>
+            {children()}
+        </div>
+    }
+}
+
+#[component]
+fn Item(
+    #[prop(into)] name: String,
+    #[prop(into)] error: Signal<String>,
+    children: Children,
+) -> impl IntoView {
+    view! {
+        <div class="transaction-display-row">
+            <div class="display-row-label">
+                {name}
+            </div>
+            {children()}
+        </div>
+        <ErrorBox error=error />
     }
 }
