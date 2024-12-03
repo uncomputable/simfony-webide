e5c8976f2b6707f8df4b1603e1a5c073434b281f
Daniel Petersen <danielpetersen@blockstream.com>
2024-12-02 18:46:31 -0800

    footer centered

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -3,24 +3,30 @@ use leptos::{component, view, IntoView};
 #[component]
 pub fn Footer() -> impl IntoView {
     view! {
-        <div class="center">
-            <h1 class="intro-title">Further Links</h1>
-            <div>
-                "🚀 "
-                <a href="https://simfony-lang.com/" target="blank">Simfony Lander</a>
-            </div>
-            <div>
-                "📜 "
-                <a href="https://docs.rs/simfony-as-rust/latest/simfony_as_rust/jet/index.html" target="blank">Jet documentation</a>
-            </div>
-            <div>
-                "🛠️ "
-                <a href="https://github.com/uncomputable/simfony-webide" target="blank">Simfony web IDE GitHub repository</a>
-            </div>
-            <div>
-                "🛠️ "
-                <a href="https://github.com/BlockstreamResearch/simfony" target="blank">Simfony compiler GitHub repository</a>
-            </div>
+        <div class="footer center">
+            <img class="footer-logo" src="images/simfony_logo.svg" />
+
+            <h1 class="footer-title">Learn more</h1>
+            
+                <div class="footer-links">
+
+                    <div class="footer-link">
+                        "🚀 "
+                        <a href="https://simfony-lang.com/" target="blank">Simfony Lander</a>
+                    </div>
+                    <div class="footer-link">
+                        "📜 "
+                        <a href="https://docs.rs/simfony-as-rust/latest/simfony_as_rust/jet/index.html" target="blank">Jet documentation</a>
+                    </div>
+                    <div class="footer-link">
+                        "🛠️ "
+                        <a href="https://github.com/uncomputable/simfony-webide" target="blank">Simfony web IDE GitHub repository</a>
+                    </div>
+                    <div class="footer-link">
+                        "🛠️ "
+                        <a href="https://github.com/BlockstreamResearch/simfony" target="blank">Simfony compiler GitHub repository</a>
+                    </div>
+                </div>
         </div>
     }
 }
